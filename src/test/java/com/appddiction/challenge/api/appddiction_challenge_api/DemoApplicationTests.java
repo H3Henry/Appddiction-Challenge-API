42a6eeff292b5a8fdf38ab96112d27fee9ee183c
Carlo Cappellini <carloacappellini@gmail.com>
2020-08-01 17:54:50 -0500

    Initial commit

---
+++ src/test/java/com/appddiction/challenge/api/appddiction_challenge_api/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appddiction.challenge.api.appddiction_challenge_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
