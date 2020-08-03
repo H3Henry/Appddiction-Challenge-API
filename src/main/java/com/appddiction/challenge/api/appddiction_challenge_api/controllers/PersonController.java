fae52501b2a3db8747f402e2371d5da291bf5e18
travis.vela <tvela@appddictionstudio.com>
2020-08-02 19:26:50 -0500

    Update PersonController.java

--- src/main/java/com/appddiction/challenge/api/appddiction_challenge_api/controllers/PersonController.java
+++ src/main/java/com/appddiction/challenge/api/appddiction_challenge_api/controllers/PersonController.java
@@ -7,10 +7,12 @@ import com.appddiction.challenge.api.appddiction_challenge_api.repositories.Pers
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,5 +32,10 @@ public class PersonController {
     public Person savePerson(@RequestBody Person p) {
         
         return pplRepo.save(p);
+    }
+
+    @DeleteMapping(value = "/delete")
+    public void deletePerson(@RequestParam Integer id) {
+        pplRepo.deleteById(id);
     }
 }
